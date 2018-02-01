b0984bfcf9b7e241c057c87b8d4a7a348462c6e3
Rishabh <Rishi.chopra8460@gmail.com>
2018-02-01 14:50:49 +0530

    Rishabh webpage push 004

--- Site/MasterPage.master.cs
+++ Site/MasterPage.master.cs
@@ -4,22 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using Model;
 public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        MasterValueClass m = new MasterValueClass();
+        m.AddMasterValue();
         //userlogin.InnerText = "Sign Out";
         //userlogin.HRef = "Logout.aspx";
        // user.Visible = false;
         if (!string.IsNullOrEmpty(Session["id"] as string))
         {
-            userlogin.InnerText = "Sign Out";
-            userlogin.HRef = "Logout.aspx";
+            //userlogi.InnerText = "Sign Out";
+            //userlogi.HRef = "Logout.aspx";
         }
         else
         {
-            user.Visible = false;
+            //user.Visible = false;
         }
     }
 }
